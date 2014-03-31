c5d0a80136f12dbbe3fca92e16b355f293e10dae
timkieboom@gmail.com <timkieboom@gmail.com@770a134c-34ad-2cb0-696f-64aa894750ac>
2014-03-31 08:28:10 +0000

    added "stuff"

--- trunk/Android Development/AWS/AWS/src/main/java/com/eindopdracht/aws/fragment/GraphViewFragment.java
+++ trunk/Android Development/AWS/AWS/src/main/java/com/eindopdracht/aws/fragment/GraphViewFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.LinearLayout;
 import com.eindopdracht.aws.R;
-
-
+import com.jjoe64.graphview.BarGraphView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GraphView.GraphViewData;
+import com.jjoe64.graphview.GraphViewSeries;
+import com.jjoe64.graphview.LineGraphView;
 
 public class GraphViewFragment extends Fragment {
 
@@ -16,13 +19,38 @@ public class GraphViewFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(false);
+
+
     }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle instanceState) {
         final View mainLayout = inflater.inflate(R.layout.fragment_graph, parent, false);
 
+
+        // init example series data
+        GraphViewSeries exampleSeries = new GraphViewSeries(new GraphViewData[] {
+                new GraphViewData(1, 2.0d)
+                , new GraphViewData(2, 1.5d)
+                , new GraphViewData(3, 2.5d)
+                , new GraphViewData(4, 1.0d)
+        });
+
+        GraphView graphView = new LineGraphView(
+                this.getActivity() // context
+                , "GraphViewDemo" // heading
+        );
+        graphView.addSeries(exampleSeries); // data
+
+        LinearLayout layout = (LinearLayout) this.getActivity().findViewById(R.id.chart);
+        layout.addView(graphView);
+
+
+
+
+
         return mainLayout;
     }
+
 
     @Override
     public void onResume() {
